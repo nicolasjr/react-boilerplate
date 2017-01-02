d3934e3178a9d1a9fdce6910b915cab188246741
Nicolas <nicolas.jreichert@gmail.com>
2017-01-02 11:47:39 +0100

    Add few helper methods that are frequently needed throughout projects

---
+++ src/js/common/makeCancelablePromise.js
@@ -0,0 +1,21 @@
+const makeCancelablePromise = (promise) => {
+  let hasCanceled = false;
+
+  const wrappedPromise = new Promise((resolve, reject) => {
+    promise.then(val => (
+      hasCanceled ? reject({ isCanceled: true }) : resolve(val)
+    ));
+    promise.catch(error => (
+      hasCanceled ? reject({ isCanceled: true }) : reject(error)
+    ));
+  });
+
+  return {
+    promise: wrappedPromise,
+    cancel() {
+      hasCanceled = true;
+    },
+  };
+};
+
+export default makeCancelablePromise;
